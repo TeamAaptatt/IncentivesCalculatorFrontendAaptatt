26b74ab5b9e8b048421f6fe55a7266a00469735a
ranasingh1 <ranasingh061177@gmail.com>
2024-03-12 18:15:58 +0530

    token fix

--- src/routes/UserTypeCheck.jsx
+++ src/routes/UserTypeCheck.jsx
@@ -1,11 +1,13 @@
-import { getIdToken } from 'firebase/auth';
+import { getIdToken, onIdTokenChanged} from 'firebase/auth';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { auth } from '../utils/firebase/firebase';
+import { setUser } from '../utils/redux/authSlice/authSlice';
 
 const UserTypeCheck = ({ admin, user }) => {
     const [userType, setUserType] = useState(null);
-    const token = useSelector((store)=>store.auth.token?.token) || '';
+    const token = useSelector((store)=>store.auth?.token?.token) || '';
+    const dispatch =useDispatch();
     useEffect(() => {
       const checkUserType = () => {
           try {
@@ -19,7 +21,37 @@ const UserTypeCheck = ({ admin, user }) => {
       };
   
       checkUserType();
-    }, [token]); // Empty dependency array to run the effect once when the component mounts
+
+      const unsubscribe = onIdTokenChanged(auth, async (user) => {
+        if (user) {
+          const token = await getIdToken(user);
+          // Dispatch action to update the token in your Redux store
+          dispatch(setUser({token }));
+
+        }
+      });
+  
+      // Refresh token every 50 minutes
+      const intervalId = setInterval(async () => {
+        try {
+          const user = auth.currentUser;
+          if (user) {
+            const token= await getIdToken(user);
+                      dispatch(setUser({token }));
+          }
+        } catch (error) {
+          console.error('Error refreshing token:', error);
+        }
+      }, 3000000 ); // 50 minutes in milliseconds
+  
+      // Clean up interval and unsubscribe on component unmount
+      return () => {
+        clearInterval(intervalId);
+        unsubscribe();
+      };
+  
+
+    }, []); // Empty dependency array to run the effect once when the component mounts
   
     console.log(userType);
   
