6b374330505b4104e7761398d6407bc82e5ce993
jaybaba8085 <sharginjaypatel@gmail.com>
2024-03-13 00:57:52 +0530

    ui changes

--- src/components/user/Dashboard/AdminDashboard.jsx
+++ src/components/user/Dashboard/AdminDashboard.jsx
@@ -11,26 +11,34 @@ import AddIncentivePeriod from '../modules/adminDashbaordModule/AddIncentivePeri
 const AdminDashboard = () => {
   const [toggle, setToggle] = useState(false);
   const [usertoggle, setUSerToggle] = useState(false);
-  const [creatIncentive, setCreateIncentive]  = useState(false)
+  const [creatIncentive, setCreateIncentive] = useState(false);
+
+  const toggleCreateIncentive = () => {
+    setCreateIncentive((prevState) => !prevState);
+  };
 
   return (
     <div className='m-4 p-2 flex flex-col items-center '>
       <div>
-        </div>
-       <h2 className=' text-4xl m-2 uppercase font-bold'>Placement Table</h2> 
-        <AdminPlacementTable/>
-        <AddPlacementButton/>
-        <OwnerShipTable/>
-        <IncentiveCalculator/>    
-        <div>
-        <button className="bg-black m-4 hover:bg-blue-400 text-white px-4 py-2 rounded-md w-44" onClick={()=>setCreateIncentive((state)=>!state)}>{creatIncentive===true?'Close':'Create Incentive'}</button>
-        {creatIncentive && <AddIncentivePeriod/>}
+      </div>
+      <h2 className=' text-4xl m-2 uppercase font-bold'>Placement Table</h2>
+      <AdminPlacementTable />
+      <AddPlacementButton />
+      <OwnerShipTable />
+      <IncentiveCalculator />
 
-        <button className="bg-black m-4 hover:bg-blue-400 text-white px-4 py-2 rounded-md w-44" onClick={()=>setUSerToggle((state)=>!state)}>{usertoggle===true?'Close':'All Users'}</button>
-        {usertoggle && <UserList/>}
-        <button className="bg-black hover:bg-blue-400 text-white px-4 py-2 rounded-md w-44" onClick={()=>setToggle((state)=>!state)}>{toggle===true?'Close':'Create User'}</button>
-        {toggle && <CreateUserForm/>}
-        </div>
+      <div>
+        <button className="bg-black m-4 hover:bg-blue-400 text-white px-4 py-2 rounded-md w-44"
+          onClick={() => setCreateIncentive((state) => !state)}>
+          {creatIncentive === true ? 'Close' : 'Create Incentive'}
+        </button>
+        {creatIncentive && <AddIncentivePeriod handleClose={toggleCreateIncentive} />}
+
+        <button className="bg-black m-4 hover:bg-blue-400 text-white px-4 py-2 rounded-md w-44" onClick={() => setUSerToggle((state) => !state)}>{usertoggle === true ? 'Close' : 'All Users'}</button>
+        {usertoggle && <UserList />}
+        <button className="bg-black hover:bg-blue-400 text-white px-4 py-2 rounded-md w-44" onClick={() => setToggle((state) => !state)}>{toggle === true ? 'Close' : 'Create User'}</button>
+        {toggle && <CreateUserForm />}
+      </div>
 
     </div>
   )
--- src/components/user/modules/adminDashbaordModule/AddIncentivePeriod.jsx
+++ src/components/user/modules/adminDashbaordModule/AddIncentivePeriod.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { BASE_URL } from '../../../../constants/api';
 import { useSelector } from 'react-redux';
 
-const AddIncentivePeriod = () => {
+const AddIncentivePeriod = ({ handleClose }) => {
   const [formData, setFormData] = useState({
     cid: '',
     incentivePeriod: {
@@ -39,55 +39,75 @@ const AddIncentivePeriod = () => {
       });
       console.log(response.data);
       // Handle success, e.g., show a success message to the user
+      handleClose(); // Close the modal after successful submission
     } catch (error) {
       console.error(error.response.data.error);
       // Handle error, e.g., show an error message to the user
     }
   };
 
+  // Function to handle click outside the modal to close it
+  const handleOutsideClick = (e) => {
+    if (e.target === e.currentTarget) {
+      handleClose();
+    }
+  };
+
   return (
-    <div className="container mx-auto mt-8">
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto bg-gray-100 p-6 rounded-lg shadow-md">
-        <label className="block mb-4">
-          User:
-          <input
-            type="text"
-            name="cid"
-            value={formData.cid}
-            onChange={handleChange}
-            className="form-input mt-1 block w-full border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-          />
-        </label>
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75" onClick={handleOutsideClick}>
+      <div className="bg-gray-200 p-4 rounded-lg shadow-md w-1/3">
+        <form onSubmit={handleSubmit}>
+          <label className="block mb-4">
+           <h1 className="font-bold text-gray-700"> User:</h1>
+            <input
+              type="text"
+              name="cid"
+              value={formData.cid}
+              onChange={handleChange}
+              className="form-input mt-1 block w-full border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            />
+          </label>
 
-        <label className="block mb-4">
-          Start Date:
-          <input
-            type="date"
-            name="startDate"
-            value={formData.incentivePeriod.startDate}
-            onChange={handleChange}
-            className="form-input mt-1 block w-full border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-          />
-        </label>
+          <label className="block mb-4">
+          <h1 className="font-bold text-gray-700"> Start Date:</h1>
+            
+            <input
+              type="date"
+              name="startDate"
+              value={formData.incentivePeriod.startDate}
+              onChange={handleChange}
+              className="form-input mt-1 block w-full border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            />
+          </label>
 
-        <label className="block mb-4">
-          End Date:
-          <input
-            type="date"
-            name="endDate"
-            value={formData.incentivePeriod.endDate}
-            onChange={handleChange}
-            className="form-input mt-1 block w-full border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-          />
-        </label>
+          <label className="block mb-4">
+          <h1 className="font-bold text-gray-700"> End Date:</h1>
+            <input
+              type="date"
+              name="endDate"
+              value={formData.incentivePeriod.endDate}
+              onChange={handleChange}
+              className="form-input mt-1 block w-full border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            />
+          </label>
 
-        <button
-          type="submit"
-          className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 mt-4 w-full"
-        >
-          Add Incentive Period
-        </button>
-      </form>
+          <div className="flex justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 w-1/2"
+            >
+              Add Incentive Period
+            </button>
+            <button
+              type="button"
+              onClick={handleClose}
+              className="bg-gray-300 text-gray-700 p-2 rounded-md hover:bg-red-400 w-1/2 ml-2"
+            >
+              Close
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
