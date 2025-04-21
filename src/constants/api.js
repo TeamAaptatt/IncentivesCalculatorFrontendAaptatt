ca4b52bbb1d27f73b23e3f14c10e97b5f0a284a9
ranasingh <ranasingh061177@gmail.com>
2025-04-21 16:27:16 +0530

    backend url change

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://incentivescalculatorbackendaaptatt.onrender.com/api/v1/"
+export const BASE_URL = "https://incentivescalculatorbackendaaptatt-q5n5.onrender.com/api/v1/"
 // export const BASE_URL = "http://localhost:8000/api/v1/"
 // export const BASE_URL = "https://backend.ekmerabharat.com/api/v1/"
 
